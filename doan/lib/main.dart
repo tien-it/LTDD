9c09eb9f0fb2b805f9f2fc430a57232a594d6f14
thanhbadboi <93701114+thanhbadboi@users.noreply.github.com>
2022-01-04 20:29:25 +0700

    thêm love, buyafter

--- doan/lib/main.dart
+++ doan/lib/main.dart
@@ -2,26 +2,19 @@ import 'package:doan/views/AddressBook/addressnullpage.dart';
 import 'package:doan/views/AddressBook/addresspage.dart';
 import 'package:doan/views/BillInfomation/billinfonullpage.dart';
 import 'package:doan/views/BillInfomation/billinforpage.dart';
+import 'package:doan/views/Buyafter/afternullpage.dart';
+import 'package:doan/views/Buyafter/afterpage.dart';
 import 'package:doan/views/ForgotPassword/createnewpass.dart';
 import 'package:doan/views/ForgotPassword/phonepage.dart';
 import 'package:doan/views/Login/login.dart';
 import 'package:doan/views/Login/password.dart';
+import 'package:doan/views/Love/lovenullpage.dart';
+import 'package:doan/views/Love/lovepage.dart';
 import 'package:doan/views/Saw/sawnullpage.dart';
 import 'package:doan/views/Saw/sawpage.dart';
 import 'package:doan/views/SignUp/confirmphonenumber.dart';
 import 'package:doan/views/SignUp/createpass.dart';
 import 'package:doan/views/SignUp/signup.dart';
-import 'package:doan/views/home/home.dart';
-import 'package:doan/views/taikhoan/account_page.dart';
-import 'package:doan/views/taikhoan/canhan/sua_email.dart';
-import 'package:doan/views/taikhoan/canhan/sua_gioitinh.dart';
-import 'package:doan/views/taikhoan/canhan/sua_hovaten.dart';
-import 'package:doan/views/taikhoan/canhan/sua_matkhau.dart';
-import 'package:doan/views/taikhoan/canhan/sua_ngaysinh.dart';
-import 'package:doan/views/taikhoan/canhan/sua_sodienthoai.dart';
-import 'package:doan/views/taikhoan/canhan/sua_tendangnhap.dart';
-import 'package:doan/views/taikhoan/canhan/thongtincanhan.dart';
-import 'package:doan/views/taikhoan/donhang/donhang.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -31,30 +24,18 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'ĐỒ ÁN LẬP TRING ANDROID',
-      theme: ThemeData(
+      theme: ThemeData(             
         primaryColor: Colors.white,
       ),
-      initialRoute: '/',
-      routes: {
-        '/home': (context) => const homepage(),
-        '/': (context) => const AccountPage(),
-        '/thong_tin_ca_nhan': (context) => const ThongTinCaNhan(),
-        '/sua_hovaten': (context) => const Sua_HovaTen(),
-        '/sua_email': (context) => const SuaEmail(),
-        '/sua_ngaysinh': (context) => const SuaNgaySinh(),
-        '/sua_sodienthoai': (context) => const SuaSoDienThoai(),
-        '/doigioitinh': (context) => const DoiGioiTinh(),
-        '/doimatkhau': (context) => const DoiMatKhau(),
-        '/suatendangnhap': (context) => const SuaTenDangNhap(),
-        '/donhang': (context) => const DonHang(),
-
-      },
-
+      home: const LoginPage(),
       debugShowCheckedModeBanner: false,
     );
   }
 }
+
+
